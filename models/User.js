8972adf407d024597f153380b2a65a3fd9605888
itsnormsyo <itsnormsyo@gmail.com>
2020-09-25 12:04:24 -0700

    first commit

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+// destructing for your convenience const Schema = mongoose.Schema; = const { Schema } = mongoose
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  googleId: String,
+});
+
+mongoose.model("users", userSchema); // <---- load into  mongoose 2 arguments
+
---
+++ routes/authRoutes.js
@@ -0,0 +1,20 @@
+const passport = require("passport");
+
+module.exports = (app) => {
+  app.get(
+    "/auth/google/",
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+    })
+  );
+
+  app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
+};
+
+// this initiates the passport to authenticate using this route.. using google (there is mini code in GoogleStrategy that uses the 'google' string)
+
+// scope specifies to google servers what access we want to have in the users profile information google has internal list of scopes
