143b7d2f6acf9e9977b3303a7cda77fe50a3f222
itsnormsyo <itsnormsyo@gmail.com>
2020-10-05 11:25:22 -0700

    sg integration test completed

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 const recipientSchema = require("../models/Recipient");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
 const Survey = mongoose.model("surveys");
 
@@ -19,6 +21,9 @@ module.exports = (app) => {
       _user: req.user.id,
       dateSent: Date.now(),
     });
+
+    //Great place to send an email
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    mailer.send();
   });
 };
- 
